5e33a6a6b1b93d3a557f4f8e38b0e8806c6e5643
David <118456832+Spamdfsa@users.noreply.github.com>
2024-12-11 21:37:18 +0100

    Update Pacman.py

--- Pacman.py
+++ Pacman.py
@@ -8,24 +8,12 @@ import random
 # initialisieren von pygame
 pygame.init()
 
-# Spielkonfiguration
-SCALING = 1
-COLORS = {
-    "background": (0, 0, 20),
-    "coin": (255, 244, 79),
-    "pacman": (255, 255, 0),
-    "geist1": (255, 0, 0),
-    "geist2": (0, 255, 255)
-}
-
 # Initialisierung
 def screen_draw(WIDTH, HEIGHT):
     HEIGHT += HEIGHT * .15
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Pac-Man")
     return screen
-
-clock = pygame.time.Clock()
 
 # Musik/Soundeffekte einrichten
 def essen_sound():
@@ -57,10 +45,10 @@ def map_einlesen(WIDTH,HEIGHT):
 def draw_text(text, font, color, screen, x, y):
     img = font.render(text, True, color)
     screen.blit(img, (x,y))
-def background_video():
+def background_video_load(SCALING):
     HEIGHT = 600*SCALING
     WIDTH = 1066*SCALING
-    frame_folder = "compressed_frames"
+    frame_folder = "background_video"
     frame_files = sorted(os.listdir(frame_folder))
     frames = []
     for frame_file in frame_files:
@@ -78,11 +66,12 @@ def picture_scaling(WIDTH, scale_factor, image):
     # Bild skalieren
     return pygame.transform.scale(image, (target_width, target_height))
     
-def menu(frames):
+def menu(frames, clock, SCALING):
     HEIGHT = 600 * SCALING
     WIDTH = 1066 * SCALING
     running = True
     frame_index = 0
+    font = pygame.font.SysFont("arial", 30)
 
     start_button = pygame.image.load("buttons/start_button.png")
     leave_button = pygame.image.load("buttons/leave_button.png")
@@ -114,8 +103,17 @@ def menu(frames):
                 elif leave_button_rect.collidepoint(event.pos):  # Prüft, ob die Klickposition im Leave-Button-Rechteck ist
                     return False
                 elif highscore_button_rect.collidepoint(event.pos):  # Prüft, ob die Klickposition im Highscore-Button-Rechteck ist
-                    error = pygame.mixer.Sound('Musik/error.wav')
-                    pygame.mixer.Sound.play(error)
+                    #error = pygame.mixer.Sound('Musik/error.wav')
+                    #pygame.mixer.Sound.play(error)
+                    highscore_file = "highscore/highscores.json"
+                    highscores = []
+                    if os.path.exists(highscore_file):
+                        with open(highscore_file, "r") as file:
+                            highscores = json.load(file)
+                    else:
+                        with open(highscore_file, "w") as file:
+                            json.dump([], file)
+                    show_highscores(screen, highscores, font, WIDTH, HEIGHT,frames, clock,SCALING)
                     
 
         # Frame anzeigen
@@ -134,6 +132,83 @@ def menu(frames):
         clock.tick(24)
 
     return True
+
+#highscore eingeben 
+def highscore_menu(score,frames, clock,SCALING):
+    HEIGHT = 600 * SCALING
+    WIDTH = 1066 * SCALING
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("highscore_eingabe")
+    font = pygame.font.SysFont("arial", 30)
+    input_active = True
+    user_name = ""
+
+    highscore_file = "highscore/highscores.json"
+    highscores = []
+
+    if os.path.exists(highscore_file):
+        with open(highscore_file, "r") as file:
+            highscores = json.load(file)
+    else:
+        with open(highscore_file, "w") as file:
+            json.dump([], file)
+    while input_active:
+        screen.fill((0,0,20))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:  # Eingabe abgeschlossen
+                    input_active = False
+                elif event.key == pygame.K_BACKSPACE: #letzes zeichen löschen
+                    user_name = user_name[:-1]
+                else:
+                    if len(user_name) < 10:  # Name begrenzen
+                        user_name += event.unicode
+
+        # Eingabetext anzeigen
+        input_text = font.render(f"Enter your name: {user_name}", True, (255, 255, 255))
+        screen.blit(input_text, (WIDTH*0.4, HEIGHT / 3))
+
+        pygame.display.flip()
+
+    # Highscore speichern
+    highscores.append({"name": user_name, "score": score})
+    highscores = sorted(highscores, key=lambda x: x["score"], reverse=True)  # Nach Score sortieren
+
+    # Zurück in die JSON-Datei schreiben
+    with open(highscore_file, "w") as file:
+        json.dump(highscores, file, indent=4)
+
+    # Highscore-Liste anzeigen
+    show_highscores(screen, highscores, font, WIDTH, HEIGHT, frames,clock,SCALING)
+
+#highscore anzeigen
+def show_highscores(screen, highscores, font, WIDTH, HEIGHT,frames,clock,SCALING):
+    running = True
+    COLORS = {
+    "background": (0, 0, 20),
+    }
+    pygame.display.set_caption("highscore")
+    while running:
+        screen.fill((COLORS["background"]))
+        title = font.render("Highscores", True, (255, 255, 0))
+        screen.blit(title, (WIDTH // 2 - title.get_width() // 2, 50))
+
+        for i, entry in enumerate(highscores[:10]):  # Zeige die Top 10
+            text = font.render(f"{i + 1}. {entry['name']} - {entry['score']}", True, (255, 255, 255))
+            screen.blit(text, (100, 150 + i * 40))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                running = False
+               
+
+        pygame.display.flip()
+    menu(frames, clock, SCALING) 
+
+    
 # Muenzen Zeichnen
 def draw_grid(screen, level, tile_size, colors):
     # offest um münzen zu zentrieren von -8
@@ -146,13 +221,12 @@ def draw_grid(screen, level, tile_size, colors):
                 pygame.draw.circle(screen, colors["coin"], (x + tile_size // 2, y + tile_size // 2 + offset_y), tile_size // 4)
             if level[row][col] == 3:
                 pygame.draw.circle(screen, colors["coin"], (x + tile_size // 2, y + tile_size // 2 + offset_y), tile_size // 2)
-#added tint
+#tint for firghtend mode
 def tint_image_blue(image):
     tinted_image = image.copy()
     tinted_image.fill((100, 100, 255, 255), special_flags=pygame.BLEND_RGBA_MULT)
     return tinted_image
 
-#added updatet ganze funkion
 def move_ghost(player_pos, ghost_direction, ghost_pos, level, frightened_mode):
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     rows, cols = len(level), len(level[0])
@@ -160,7 +234,7 @@ def move_ghost(player_pos, ghost_direction, ghost_pos, level, frightened_mode):
     closest_direction = None
     min_distance = float('inf')
 
-    # Aktuelle Richtung reversen
+    # Rückwärtsrichtung zum ausschließen bestimmen
     reverse_direction = (-ghost_direction[0], -ghost_direction[1])
 
     for direction in directions:
@@ -203,8 +277,9 @@ def move_player(player_pos, direction, input_direction, level):
         new_pos = [player_pos[0] + direction[0], player_pos[1] + direction[1]]
         if (0 <= new_pos[0] < rows and 0 <= new_pos[1] < cols and level[new_pos[0]][new_pos[1]] not in [1, 5]):
             player_pos[:] = new_pos
-#added
-def check_player_collision(ghost_positions, player_pos, frightened_mode, level):
+
+
+def check_player_collision(ghost_positions, player_pos, frightened_mode, level, score):
     got_hit = False
 
     ghost_house = [(row, col) for row in range(len(level)) 
@@ -219,6 +294,7 @@ def check_player_collision(ghost_positions, player_pos, frightened_mode, level):
         if abs(ghost_pos[0] - player_pos[0]) <= 1 and abs(ghost_pos[1] - player_pos[1]) <= 1:
             if frightened_mode:
                 ghost_pos[:] = random.choice(ghost_house)
+                score += 500
             else:
                 frightened_mode = True
                 got_hit = True
@@ -227,7 +303,7 @@ def check_player_collision(ghost_positions, player_pos, frightened_mode, level):
         for ghost_pos in ghost_positions:
             ghost_pos[:] = random.choice(ghost_house)
 
-    return frightened_mode, got_hit
+    return frightened_mode, got_hit, score
 
 
 
@@ -236,7 +312,7 @@ def update_game(player_pos, ghost1_pos, ghost2_pos, direction, ghost1_direction,
     move_ghost(player_pos, ghost1_direction, ghost1_pos, level, frightened_mode)
     move_ghost(player_pos, ghost2_direction, ghost2_pos, level, frightened_mode)
 
-    # added Teleportieren bei Tunnel 4
+    # Teleportieren bei Tunnel 4
     if level[player_pos[0]][player_pos[1]] == 4:
         teleport_positions = []
         for row in range(len(level)):
@@ -249,23 +325,23 @@ def update_game(player_pos, ghost1_pos, ghost2_pos, direction, ghost1_direction,
             new_pos = random.choice(teleport_positions)
             player_pos[:] = new_pos
             direction[:] = (-direction[0], -direction[1])
-
-    #added
+    
+    #checks if player collects big coin
     if level[player_pos[0]][player_pos[1]] == 3:
         frightened_count = 0
         frightened_mode = True
         ghost1_direction[:] =(-ghost1_direction[0], -ghost1_direction[1])
         ghost2_direction[:] =(-ghost2_direction[0], -ghost2_direction[1])
-
+    
+    #checks for sound
     if level[player_pos[0]][player_pos[1]] == 2 or level[player_pos[0]][player_pos[1]] == 3:
         level[player_pos[0]][player_pos[1]] = 0
         score += 10
         zeit_jetzt = time.time()
+        #sounds nicht überlagern
         if zeit_jetzt >= zeit + 0.717:
             pygame.mixer.Sound.play(essen)
-            score += 1
             zeit = time.time()
-    #added return
 
     return score, zeit, frightened_mode, frightened_count
 
@@ -300,34 +376,53 @@ def start_ghost_pos(level):
     else:
         print("Keine Startposition (0, da keine Münze) für die Geister gefunden")
         return [0, 0]
-    
+
+def check_for_w(level):
+    win = True
+    for row in range(len(level)):
+        for col in range(len(level[row])):
+            if level[row][col] == 2:
+                win = False
+    return win
 
 def read_level():
-    with open('.\Karte\map1.json', 'r') as file:
+    with open('Karte/map.json', 'r') as file:
         data = json.load(file)
     return data
 
 def main():
+
     pygame.init()
+
     zeit = time.time()
+
+
+    # Spielkonfiguration
+    SCALING = 1
+
     TILE_SIZE = 20*SCALING
     WIDTH, HEIGHT = 560*SCALING, 600*SCALING
     running = True
     player_lives = 3
-
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption("PACMAN")
-
-    frames = background_video()
+    COLORS = {
+        "background": (0, 0, 20),
+        "coin": (255, 244, 79),
+    }
+    frames = background_video_load(SCALING)
     #Sounds einlesen
     essen = essen_sound()
     #Menu
-    running = menu(frames)
-    #added clock_tick
+    running = menu(frames, clock,SCALING)
+    #clock_tick
     clock_ticks = 10
+    font = pygame.font.SysFont("arial", 30)
 
     # Abspielen des Geistes
     Picture_Frame = 0
+
     # Verschieben des Geistbildes
     Pixel_movement_right = 10 * SCALING
     Pixel_movement_up = 0
@@ -350,7 +445,7 @@ def main():
     direction = [0, 1]
     input_direction = [0, 0]
     score = 0
-    #added frightened_mode
+    #frightened_mode
     frightened_mode = False
     frightened_duration = 10 * clock_ticks
     frightened_count = 0
@@ -359,6 +454,7 @@ def main():
 
     
     while running:
+   
         # Screen definieren
         screen = screen_draw(WIDTH, HEIGHT)
 
@@ -411,7 +507,7 @@ def main():
         elif keys[pygame.K_RIGHT] | keys[pygame.K_d]:
             input_direction = [0, 1]
 
-        #added frightened_mode turn off and on
+        #frightened_mode turn off and on
         if frightened_mode:
             if frightened_count < frightened_duration:
                 frightened_count +=1
@@ -425,20 +521,51 @@ def main():
                 frightened_mode = True
                 frightened_count = 0
         #has a list of the ghost_pos
-        frightened_mode, got_hit = check_player_collision([ghost1_pos, ghost2_pos], player_pos, frightened_mode, level)
+        frightened_mode, got_hit, score = check_player_collision([ghost1_pos, ghost2_pos], player_pos, frightened_mode, level, score)
         if got_hit:
             got_hit = False
             player_lives -= 1
             print("live lost")
             if 1 > player_lives:
                 print("Game Over")
-                running = menu(frames)
-                
-        #added chase and frightened_count zum reseten des cooldowns
+                player_lives = 3
+                highscore_menu(score,frames, clock,SCALING)
+                # Pacman und Geister einlesen
+                pacman_idle, pinky, blinky = Pacman_Ghost_Image_Load(TILE_SIZE)
+                # Map
+                map = map_einlesen(WIDTH,HEIGHT)
+                level = read_level()
+                player_pos = [1, 1]
+                ghost1_direction = [0, 0]
+                ghost1_pos = start_ghost_pos(level)
+                ghost2_direction = [0, 0]
+                ghost2_pos = start_ghost_pos(level)
+                direction = [0, 1]
+                input_direction = [0, 0]
+                score = 0
+        win = check_for_w(level)
+
+        if win or (event.type == pygame.KEYDOWN and event.key == pygame.K_HOME):
+            # Pacman und Geister einlesen
+            pacman_idle, pinky, blinky = Pacman_Ghost_Image_Load(TILE_SIZE)
+            # Map
+            map = map_einlesen(WIDTH,HEIGHT)
+            level = read_level()
+            player_pos = [1, 1]
+            ghost1_direction = [0, 0]
+            ghost1_pos = start_ghost_pos(level)
+            ghost2_direction = [0, 0]
+            ghost2_pos = start_ghost_pos(level)
+            direction = [0, 1]
+            input_direction = [0, 0]
+            clock_ticks += 1
+
         score, zeit, frightened_mode, frightened_count  = update_game(
             player_pos, ghost1_pos, ghost2_pos, direction, ghost1_direction, ghost2_direction, input_direction, level, score, zeit, essen, frightened_mode, frightened_count)
-        font = pygame.font.SysFont("arial", 30)
-        draw_text(str(score),font ,COLORS["coin"],screen, int(WIDTH - 60*SCALING),int(HEIGHT + 20*SCALING))
+
+        
+        draw_text(str(score),font ,COLORS["coin"],screen, int(WIDTH - 100*SCALING),int(HEIGHT + 20*SCALING))
+        draw_text(("Lifes " + str(player_lives)), font, COLORS["coin"], screen, int(60*SCALING), int(HEIGHT +20*SCALING))
         pygame.display.flip()
         clock.tick(clock_ticks)
 
